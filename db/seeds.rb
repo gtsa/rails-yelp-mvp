d69f68928c9f4bcdbfad41ab3858ba67e5a78877
George Tsagiannis <gtsagiannis@gmail.com>
2022-11-16 22:55:50 +0000

    Added challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name:'Ricardo', address:'Paris 13', phone_number:'07 82 67 89 09', category:'italian')
+Restaurant.create(name:'Chez Richard', address:'Paris 14', phone_number:'01 82 67 89 09', category:'french')
+Restaurant.create(name:'Tama', address:'Paris 15', phone_number:'06 82 67 89 09', category:'belgian')
+Restaurant.create(name:'Origami', address:'Paris 16', phone_number:'07 83 67 89 09', category:'japanese')
+Restaurant.create(name:'Wong Tan', address:'Paris 17', phone_number:'07 89 67 89 09', category:'chinese')
